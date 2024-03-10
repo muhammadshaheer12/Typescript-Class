079ff308d6cd9133dd80fab4eb8522b68560dd46
Muhammad Shaheer <159147097+muhammadshaheer12@users.noreply.github.com>
2024-03-10 23:07:11 +0500

    commit

---
+++ main.ts
@@ -0,0 +1,40 @@
+// core data types
+// 1) string
+let stringtypeexample = "this is a string";
+console.log(stringtypeexample);
+// 2) number
+let numbertypeexample = 100;
+console.log(numbertypeexample);
+// 3) boolean true and false
+let booleantypeexample1 = true;
+console.log(booleantypeexample1);
+let booleantypeexample2 = false;
+console.log(booleantypeexample1);
+// variables
+// variable syntax
+// let variablename: type = value;
+// const constantname: type = value;
+// 1) var
+var message = "1234"; // '' , "" string
+console.log(message);
+var messagenew = "234";
+console.log(messagenew);
+var messagenew = "333";
+console.log(messagenew);
+// 2) let
+let favouritecolour = "yellow";
+console.log(favouritecolour);
+favouritecolour = "black";
+console.log(favouritecolour);
+// 3) const
+const messagenew1 = "789";
+console.log(messagenew1);
+// strong typing
+// let variablename: type = value;
+// const constantname: type = value;
+let username: string = "ayesha";
+console.log(username);
+let age: number = 80;
+console.log(age);
+let ayesha_alive: boolean = true;
+console.log(ayesha_alive);
